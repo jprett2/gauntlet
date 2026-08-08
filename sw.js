3fd5d741b46edb69f88ffdaf9550e98fd15d80cf
jprett2 <jprett2@gmail.com>
2026-08-08 09:13:01 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gauntlet-v1";
+const CACHE = "gauntlet-v2";
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./icon.png"])));
   self.skipWaiting();
