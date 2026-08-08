639fddc0bfd0ee0520fcddd304af4477c1c3e625
jprett2 <jprett2@gmail.com>
2026-08-08 08:03:14 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "gauntlet-v1";
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./icon.png"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request).then((resp) => {
+      const copy = resp.clone();
+      if (resp.ok && new URL(e.request.url).origin === location.origin) {
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+      }
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
