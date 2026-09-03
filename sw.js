e11881fdd91c3b280fc8fed5fa13426d5d7d1c19
jprett2 <jprett2@gmail.com>
2026-09-03 14:02:59 -0500

    Deploy site v7: configurable 2-4 games per screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gauntlet-v6";
+const CACHE = "gauntlet-v7";
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./icon.png"])));
   self.skipWaiting();
