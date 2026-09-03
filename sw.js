cae821e338ec6624d24cf828aa313ea7edd0f534
jprett2 <jprett2@gmail.com>
2026-09-03 14:14:42 -0500

    Deploy site v8: 2x2 grid layout with larger covers for multi-game screens

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gauntlet-v7";
+const CACHE = "gauntlet-v8";
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./icon.png"])));
   self.skipWaiting();
